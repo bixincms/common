07adb7d836fa5f6c8ffcfe4ece92574d2f612b58
gogogo <gogogo@gmail.com>
2019-09-06 09:43:11 +0800

    添加文件下载函数

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # 放一些常用的函数
 ## img.go
 Base64 转成图片 `ImgBase64ToImg`  
-图片转换成Base64 `ImgToBase64` 
-##download.go 
+图片转换成Base64 `ImgToBase64`   
+## download.go  
 文件下载 `Download`
--- download.go
+++ download.go
@@ -6,12 +6,19 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func Download(url string, name string) (n int64, err error) {
+	resp, err := http.Get(url)
+	content_type := resp.Header.Get("Content-Type")
+	split := strings.Split(content_type, "/")
+
+	if len(split) == 2 {
+		name = name[:strings.LastIndex(name, ".")] + "." + split[1]
+	}
 	out, err := os.Create(name)
 	defer out.Close()
-	resp, err := http.Get(url)
 	defer resp.Body.Close()
 	pix, err := ioutil.ReadAll(resp.Body)
 	n, err = io.Copy(out, bytes.NewReader(pix))
